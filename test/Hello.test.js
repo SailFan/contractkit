334a898fae05ffc49a5fe38aca45f582743740ea
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-06-30 16:33:33 +0800

    lesson 1

--- test/Hello.test.js
+++ test/Hello.test.js
@@ -1,5 +1,18 @@
 const {expect} = require("chai")
+const { ethers } = require("hardhat");
 
+
+
+describe("Hello", function(){
+    it("Test Hello",async function () {
+        const deployedAddress = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"; 
+        const  helloFactory =  await ethers.getContractFactory('Hello');
+        const hello = helloFactory.attach(deployedAddress);
+        const greeting = await hello.sayHello();
+        console.log(`这个是我拿到的: ${greeting}`);
+
+    })
+})
 
 
 
